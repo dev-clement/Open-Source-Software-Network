3c73a5fd172bcfb5f09f8518d47aa85db3421c3a
Clement FRANCOIS <clement.francois.developpeur@outlook.com>
2026-04-07 19:13:37 +0200

    fix(typo) Fix typo in the python comment of the __init__ of db module

--- backend/app/db/__init__.py
+++ backend/app/db/__init__.py
@@ -2,6 +2,6 @@ from .session import DatabaseEngine
 from . import models
 
 # __all__ tells Python exactly what should be exported.
-# We include 'DatabaseEngine' and for easy access, 
-# and 'models' to ensure they are discovered by SQLModel/Alembic.
+# We include 'DatabaseEngine' for easy access
+# as well as 'models' to ensure they are discovered by SQLModel/Alembic.
 __all__ = ["DatabaseEngine", "models"]
