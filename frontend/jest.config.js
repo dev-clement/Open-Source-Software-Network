dc2871d455df78df4f36331be85700107f91d4b3
Clement FRANCOIS <clement.francois.developpeur@outlook.com>
2026-05-04 14:07:17 +0200

    chore(test): initialize Jest for unit testing
    - Added Jest and related testing libraries for React/Next.js unit tests
    - Created initial Jest configuration files
    - Added "test" script to package.json to run tests with Jest

---
+++ frontend/jest.config.js
@@ -0,0 +1,14 @@
+const { createDefaultPreset } = require('ts-jest');
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    // Add more aliases if needed
+  },
+};
